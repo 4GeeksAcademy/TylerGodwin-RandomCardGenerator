0f72bc63a6f54fbec1c9ddb710ae69998767bcab
Tgodwin94 <tyler.godwin94@gmail.com>
2024-09-14 02:09:18 +0000

    TylerGodwin-RandomCardGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomRank();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateRandomRank = () => {
+  let rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomRank = Math.floor(Math.random() * rank.length);
+  return rank[randomRank];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomSuit = () => {
+  let suit = ["spade", "heart", "club", "diamond"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
 };
